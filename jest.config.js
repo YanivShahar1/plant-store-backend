edf297f0c657ac1f6ed3806f72859de708407826
YanivShahar <yanivshahar97@gmail.com>
2024-12-17 22:17:27 +0200

    Added Jest configuration file

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+// backend/jest.config.js
+module.exports = {
+    testEnvironment: 'node',
+    setupFilesAfterEnv: ['./__tests__/setup.js'],
+    testMatch: ['**/__tests__/**/*.test.js'],
+    verbose: true
+  };
